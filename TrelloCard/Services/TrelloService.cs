8a86bc8f18a81bf36df28209796d1eb0586313a8
Vikram Singh Saini <vssaini.dev@gmail.com>
2023-06-10 08:28:52 +0530

    Attach image and videos to Trello card

--- TrelloCard/Services/TrelloService.cs
+++ TrelloCard/Services/TrelloService.cs
@@ -1,7 +1,10 @@
 ﻿using Manatee.Trello;
+using System;
 using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Web;
 using TrelloCard.Contracts;
 
 namespace TrelloCard.Services
@@ -22,13 +25,21 @@ namespace TrelloCard.Services
             return card;
         }
 
+        private async Task<bool> IsPremiumMember()
+        {
+            // Ref - https://community.atlassian.com/t5/Trello-questions/Is-there-any-API-available-to-get-check-trello-user-account-is/qaq-p/641392
+
+            throw new NotImplementedException();           
+            
+        }
+
         private static async Task<IBoard> GetSpecificBoardAsync()
         {
             var auth = GetTrelloAuthorization();
 
             var factory = new TrelloFactory();
             var me = await factory.Me(auth);
-
+                        
             // Refreshing is must
             await me.Boards.Refresh();
 
@@ -63,7 +74,50 @@ namespace TrelloCard.Services
             var card = await targetList.Cards
                 .Add("Complete Trello API task", "We must complete this task as soon as possible", dueDate: null);
 
+           await AttachImageAndVideoFilesAsync(card);
+
             return card;
+        }
+
+        private static async Task AttachImageAndVideoFilesAsync(ICard card)
+        {
+            // Ref - https://support.atlassian.com/trello/docs/adding-attachments-to-cards/
+            // There is a 10 MB file upload limit per attachment. However, paid members have a 250 MB file upload limit per attachment. There is no account data storage limit in any Trello plan.
+
+            // The 10MB file upload only applies to files uploaded from your computer.It does not apply to files attached from Google Drive, Dropbox, Box or OneDrive.
+
+            var imgBytes = GetImageBytes();
+            await card.Attachments.Add(imgBytes, "Logo.png");
+
+            var videoBytes = GetVideoBytes();
+            await card.Attachments.Add(videoBytes, "Video.mp4");
+        }
+
+        private static byte[] GetImageBytes()
+        {
+            var imagePath = HttpContext.Current.Server.MapPath("~/Attachments/Logo.png");
+            return File.ReadAllBytes(imagePath);
+        }
+
+        private static byte[] GetVideoBytes()
+        {
+            // NOTE - This commented code can be used for process directly from request
+            //    using (Stream requestStream = request.GetRequestStream())
+            //    using (Stream video = File.OpenRead("Path"))
+            //    {
+            //        byte[] buffer = new byte[4096];
+
+            //        while (true)
+            //        {
+            //            int bytesRead = video.Read(buffer, 0, buffer.Length);
+
+            //            if (bytesRead == 0) break;
+            //            requestStream.Write(buffer, 0, bytesRead);
+            //        }
+            //    }
+
+            var videoFilePath = HttpContext.Current.Server.MapPath("~/Attachments/Video.mp4");
+            return File.ReadAllBytes(videoFilePath);
         }
     }
 }
